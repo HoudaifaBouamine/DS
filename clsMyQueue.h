bd6a94f9d0ef485036e15a2bc2702c74936bd3a9
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-04-30 16:17:17 +0100

    Creating queue ds

--- clsMyQueue.h
+++ clsMyQueue.h
@@ -9,9 +9,45 @@ class clsMyQueue
 
 private:
 
-	clsDblLinkedList<T>* _list = NULL;
+	clsDblLinkedList<T> _list;
 
 public :
+
+	void push(T value) {
+
+
+		_list.InsertAtEnd(value);
+
+	}
+
+	void pop() {
+
+		_list.DeleteFirstNode();
+	}
+
+	T front() {
+
+		return _list.GetItem(0);
+	}
+
+	T back() {
+
+		return _list.GetItem(size() - 1);
+	}
+
+	void print() {
+		_list.PrintList();
+	}
+
+	int size() {
+
+		return _list.Size();
+	}
+
+	bool is_empty() {
+
+		return _list.IsEmpty();
+	}
 
 
 
